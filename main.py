fefb820c1a532b82a29b0aff5dfd66080b8191f1
Joao Mateus <Joao.Mateus.T0133534@edu.atec.pt>
2026-04-28 18:19:06 +0100

    Adicionado Sub Menu Listar apenas graficamente

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ def mostrarMenu():
 alunos = []
 
 def adicionarAluno(lstAlunos:list):
-    novoId = len(alunos) + 1
+    novoId = str(len(alunos) + 1)
+    print("+------------+")
+    print("|NOVO REGISTO|")
+    print("+------------+\n")
     nome = input("Nome: ")
     email = input("Email: ")
     telefone = input("Telefone: ")
@@ -25,8 +28,19 @@ def adicionarAluno(lstAlunos:list):
     print(f"\nO Aluno {aluno["Nome"]} foi registado com sucesso com o ID: {aluno["Id"]}!")
 
 
+
 def listarAlunos(lstAlunos:list):
+
     if len(lstAlunos) > 0:
+
+        print("+-----------MENU LISTAR----------+")
+        print("| 1 - Listar os Alunos por ID    |")
+        print("| 2 - Listar os Alunos por nome  |")
+        print("| 3 - Pesquisar Aluno por ID     |")
+        print("| 4 - Pesquisar Aluno por nome   |")
+        print("| 5 - Listar todos os Alunos     |")
+        print("+--------------------------------+")
+
         for aluno in lstAlunos:
             print(f"\nID: {aluno["Id"]} | Nome: {aluno["Nome"]} | Email: {aluno["Email"]} | Telefone: {aluno["Telefone"]} | Data Nascimento: {aluno["DataDeNascimento"]} | Nota: {aluno["Nota"]}")
     else:
@@ -39,6 +53,19 @@ def listarAlunos(lstAlunos:list):
         if resposta == "s":
             os.system("cls")
             adicionarAluno(lstAlunos)
+
+def atualizarRegisto(lstAlunos:list):
+    idResposta = input("Indique o id do Aluno que pretende atualizar: ")
+    atualizar = ""
+    while atualizar != "s":
+        i=0
+        if lstAlunos[i]["Id"] == idResposta:
+            print(f"ID: {lstAlunos[i]["Id"]} | Nome: {lstAlunos[i]["Nome"]} | Email: {lstAlunos[i]["Email"]} | Telefone: {lstAlunos[i]["Telefone"]} | Data Nascimento: {lstAlunos[i]["DataDeNascimento"]} | Nota: {lstAlunos[i]["Nota"]}")
+            atualizar = input("Pretnde editar este aluno?")
+        i+=1
+    for atributo in lstAlunos[int(idResposta)]:
+        resposta = input(f"Pretende atulizar {atributo} do Aluno?")
+        
 
 
 while True:
@@ -54,6 +81,8 @@ while True:
             os.system("cls")
             listarAlunos(alunos)
             input()
+        case "3":
+            atualizarRegisto(alunos)
         case "5":
             break
         case _:
